6b21333aad36e6caa4f5b49647f49eb6e45f7f92
EduarteXD <i@m.oxdl.cn>
2025-05-06 02:55:08 +0800

    style: wrap upgrade mapping logic in Hanger component

--- src/pages/CCUPlanner/components/Hanger.tsx
+++ src/pages/CCUPlanner/components/Hanger.tsx
@@ -76,90 +76,92 @@ export default function Hanger({ ships, onDragStart }: ShipSelectorProps) {
           <Refresh className={isRefreshing ? 'animate-spin' : ''} />
         </IconButton>
       </div>
-      {hangarExpanded && (
-        upgrades.length > 0 ?
-          upgrades.map(upgrade => {
-            const from = upgrade.name.split("to")[0].split("-")[1].trim().toUpperCase()
-            const to = (upgrade.name.split("to")[1]).trim().split(" ").slice(0, -2).join(" ").toUpperCase()
+      <div className="max-h-[calc(100vh-500px)] overflow-y-auto">
+        {hangarExpanded && (
+          upgrades.length > 0 ?
+            upgrades.map(upgrade => {
+              const from = upgrade.name.split("to")[0].split("-")[1].trim().toUpperCase()
+              const to = (upgrade.name.split("to")[1]).trim().split(" ").slice(0, -2).join(" ").toUpperCase()
 
-            const fromShip = ships.find(ship => ship.name.toUpperCase() === from)
-            const toShip = ships.find(ship => ship.name.toUpperCase() === to)
+              const fromShip = ships.find(ship => ship.name.toUpperCase() === from)
+              const toShip = ships.find(ship => ship.name.toUpperCase() === to)
 
-            if (!fromShip || !toShip) {
-              return <></>
-            }
+              if (!fromShip || !toShip) {
+                return <></>
+              }
 
-            return <div
-              key={fromShip.id + toShip.id}
-              className="flex flex-col w-full items-center justify-center pt-2 pb-1 gap-2 border-b border-gray-200 last:border-b-0"
-            >
-              <div className="text-xs text-gray-400">
-                {upgrade.name}
-              </div>
-
-              <div
-                draggable
-                onDragStart={(event) => onDragStart(event, fromShip)}
-                className="p-2 cursor-move transition-colors hover:bg-amber-100 w-full"
+              return <div
+                key={fromShip.id + toShip.id}
+                className="flex flex-col w-full items-center justify-center pt-2 pb-1 gap-2 border-b border-gray-200 last:border-b-0"
               >
-                <div className="flex items-center text-left">
-                  <img
-                    src={fromShip.medias.productThumbMediumAndSmall}
-                    alt={fromShip.name}
-                    className="w-16 h-16 object-cover mr-2"
-                  />
-                  <div>
-                    <div className="flex items-center gap-2">
-                      <h3 className="font-medium">{fromShip.name}</h3>
+                <div className="text-xs text-gray-400">
+                  {upgrade.name}
+                </div>
+
+                <div
+                  draggable
+                  onDragStart={(event) => onDragStart(event, fromShip)}
+                  className="p-2 cursor-move transition-colors hover:bg-amber-100 w-full"
+                >
+                  <div className="flex items-center text-left">
+                    <img
+                      src={fromShip.medias.productThumbMediumAndSmall}
+                      alt={fromShip.name}
+                      className="w-16 h-16 object-cover mr-2"
+                    />
+                    <div>
+                      <div className="flex items-center gap-2">
+                        <h3 className="font-medium">{fromShip.name}</h3>
+                      </div>
+                      <div className="text-xs text-gray-400">{fromShip.manufacturer.name}</div>
+                      <div className="text-sm text-blue-400 font-bold flex items-center gap-2">
+                        <span>{(fromShip.msrp / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                      </div>
                     </div>
-                    <div className="text-xs text-gray-400">{fromShip.manufacturer.name}</div>
-                    <div className="text-sm text-blue-400 font-bold flex items-center gap-2">
-                      <span>{(fromShip.msrp / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                  </div>
+                </div>
+
+                <div className="text-xs text-gray-400 flex items-center justify-between w-full px-3">
+                  <span>↓ 升级到</span>
+                  <span className="flex items-center gap-1">
+                    花费
+                    <span className="text-blue-400 font-bold">{upgrade.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                    {((toShip.msrp - fromShip.msrp) / 100) !== upgrade.value &&
+                      <span className="text-xs text-gray-400 line-through">{((toShip.msrp - fromShip.msrp) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                    }
+                  </span>
+                </div>
+
+                <div
+                  draggable
+                  onDragStart={(event) => onDragStart(event, toShip)}
+                  className="p-2 cursor-move transition-colors hover:bg-amber-100 w-full"
+                >
+                  <div className="flex items-center text-left">
+                    <img
+                      src={toShip.medias.productThumbMediumAndSmall}
+                      alt={toShip.name}
+                      className="w-16 h-16 object-cover mr-2"
+                    />
+                    <div>
+                      <div className="flex items-center gap-2">
+                        <h3 className="font-medium">{toShip.name}</h3>
+                      </div>
+                      <div className="text-xs text-gray-400">{toShip.manufacturer.name}</div>
+                      <div className="text-sm text-blue-400 font-bold flex items-center gap-2">
+                        <span>{(toShip.msrp / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-
-              <div className="text-xs text-gray-400 flex items-center justify-between w-full px-3">
-                <span>↓ 升级到</span>
-                <span className="flex items-center gap-1">
-                  花费
-                  <span className="text-blue-400 font-bold">{upgrade.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
-                  {((toShip.msrp - fromShip.msrp) / 100) !== upgrade.value &&
-                    <span className="text-xs text-gray-400 line-through">{((toShip.msrp - fromShip.msrp) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
-                  }
-                </span>
-              </div>
-
-              <div
-                draggable
-                onDragStart={(event) => onDragStart(event, toShip)}
-                className="p-2 cursor-move transition-colors hover:bg-amber-100 w-full"
-              >
-                <div className="flex items-center text-left">
-                  <img
-                    src={toShip.medias.productThumbMediumAndSmall}
-                    alt={toShip.name}
-                    className="w-16 h-16 object-cover mr-2"
-                  />
-                  <div>
-                    <div className="flex items-center gap-2">
-                      <h3 className="font-medium">{toShip.name}</h3>
-                    </div>
-                    <div className="text-xs text-gray-400">{toShip.manufacturer.name}</div>
-                    <div className="text-sm text-blue-400 font-bold flex items-center gap-2">
-                      <span>{(toShip.msrp / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            })
+            :
+            <div className="text-center text-gray-400 mb-2">
+              暂无数据 <Link href="/extension.zip" onClick={handleExtensionLinkClick}>下载浏览器扩展程序</Link>
             </div>
-          })
-          :
-          <div className="text-center text-gray-400 mb-2">
-            暂无数据 <Link href="/extension.zip" onClick={handleExtensionLinkClick}>下载浏览器扩展程序</Link>
-          </div>
-      )}
+        )}
+      </div>
       {/* 扩展程序安装说明弹窗 */}
       <ExtensionModal 
         open={extensionModalOpen} 
