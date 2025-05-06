f8578872fe7ef4f48b64f20d31e6e1cc0cd5c3a2
EduarteXD <i@m.oxdl.cn>
2025-05-06 18:39:51 +0800

    feat: add Google AdSense integration

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import App from './App.tsx'
 import { store } from './store'
 import { LocaleProvider } from './contexts/LocaleContext'
 
+const adSenseScript = document.createElement('script')
+adSenseScript.crossOrigin = 'anonymous'
+adSenseScript.async = true
+adSenseScript.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${import.meta.env.VITE_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`
+document.head.appendChild(adSenseScript)
+
+
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <LocaleProvider>
