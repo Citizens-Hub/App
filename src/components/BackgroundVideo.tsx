5ad59120af37fbe688c5033a9846835019fac879
EduarteXD <i@m.oxdl.cn>
2025-05-15 00:47:26 +0800

    style: update BackgroundVideo component

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -1,11 +1,11 @@
-import { Link } from "@mui/material";
+import { Link } from "react-router";
 
 export default function BackgroundVideo() {
   return (
     <div className='fixed top-[65px] left-0 w-full h-[calc(100vh-65px)] overflow-hidden pointer-events-none'>
       <video className='w-full h-full object-cover blur-sm scale-105 grayscale-80 opacity-80' src="/videos/bg.mp4" autoPlay muted loop playsInline />
-      <p className="absolute bottom-5 left-5 text-md opacity-80 text-gray-50 drop-shadow-lg">
-        Video by「-SMrP-」- <Link href="https://www.bilibili.com/video/BV1JHEAz6E16" target="_blank" className="underline !text-sky-100 !decoration-sky-100">BILIBILI</Link>
+      <p className="absolute bottom-5 left-5 text-md opacity-80 text-gray-50 drop-shadow-lg pointer-events-auto">
+        Video by「<Link to="https://www.bilibili.com/video/BV1JHEAz6E16" target="_blank" className="decoration-none !text-sky-100"> -SMrP- </Link>」
       </p>
     </div>
   )
