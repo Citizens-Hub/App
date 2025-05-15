735e0443eba83f792329b52dc3f7b6fdf653c976
EduarteXD <i@m.oxdl.cn>
2025-05-15 22:34:16 +0800

    style: adjust header ad dimensions

--- src/components/HeaderAd.tsx
+++ src/components/HeaderAd.tsx
@@ -2,11 +2,11 @@ import { Adsense } from '@ctrl/react-adsense';
 
 export default function HeaderAd() {
   return (
-    <div className="items-center justify-center hidden lg:flex -my-2">
+    <div className="items-center justify-center hidden lg:flex -my-10">
       <Adsense
         client={import.meta.env.VITE_ADSENSE_CLIENT}
         slot="4587258440"
-        style={{ width: 500, height: 50 }}
+        style={{ width: 500, height: 65 }}
         format=""
       />
     </div>
