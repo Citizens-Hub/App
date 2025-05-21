bfff5297d7fec35401b30dc8c46aa28c85e3822f
eduarte <eduartecliff@gmail.com>
2025-05-22 02:58:33 +0800

    refactor(main): improve error boundary fallback UI

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,23 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { ErrorBoundary } from "react-error-boundary";
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary fallback={<div className="flex flex-col gap-4 p-4">
-    <h1>Something went wrong</h1>
-    <button onClick={() => {
-      window.location.reload()
-    }}>Reload</button>
-    <button onClick={() => {
-      window.localStorage.clear()
-      window.location.reload()
-    }}>Clear Storage and Reload</button>
-    <button onClick={() => window.location.href="https://github.com/Citizens-Hub/App/issues"}>Report Issue</button>
-    <button onClick={() => window.location.href="https://discord.com/invite/AEuRtb5Vy8"}>Discord Support Channel</button>
-    </div>}>
+  <ErrorBoundary fallbackRender={({ error, resetErrorBoundary }) => {
+    return <>
+      <div className="flex flex-col gap-4 p-4">
+        <h1>Something went wrong</h1>
+        <p>{String(error)}</p>
+        <button onClick={() => {
+          resetErrorBoundary()
+        }}>Reload</button>
+        <button onClick={() => {
+          window.localStorage.clear()
+          window.location.reload()
+        }}>Clear Storage and Reload</button>
+        <button onClick={() => window.location.href = "https://github.com/Citizens-Hub/App/issues"}>Report Issue</button>
+        <button onClick={() => window.location.href = "https://discord.com/invite/AEuRtb5Vy8"}>Discord Support Channel</button>
+      </div>
+    </>
+  }}>
     <Provider store={store}>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_PUBLIC_GOOGLE_SIGNIN_CLIENT_ID}>
         <LocaleProvider>
