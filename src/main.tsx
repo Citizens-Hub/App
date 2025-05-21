3df506c89501772ca2302bda7ef06cad72f66738
eduarte <eduartecliff@gmail.com>
2025-05-22 02:53:51 +0800

    feat: add error boundary for better error handling

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,27 @@ import App from './App.tsx'
 import { store } from './store'
 import { LocaleProvider } from './contexts/LocaleContext'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ErrorBoundary } from "react-error-boundary";
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_PUBLIC_GOOGLE_SIGNIN_CLIENT_ID}>
-      <LocaleProvider>
-        <App />
-      </LocaleProvider>
-    </GoogleOAuthProvider>
-  </Provider>
+  <ErrorBoundary fallback={<div className="flex flex-col gap-4 p-4">
+    <h1>Something went wrong</h1>
+    <button onClick={() => {
+      window.location.reload()
+    }}>Reload</button>
+    <button onClick={() => {
+      window.localStorage.clear()
+      window.location.reload()
+    }}>Clear Storage and Reload</button>
+    <button onClick={() => window.location.href="https://github.com/Citizens-Hub/App/issues"}>Report Issue</button>
+    <button onClick={() => window.location.href="https://discord.com/invite/AEuRtb5Vy8"}>Discord Support Channel</button>
+    </div>}>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_PUBLIC_GOOGLE_SIGNIN_CLIENT_ID}>
+        <LocaleProvider>
+          <App />
+        </LocaleProvider>
+      </GoogleOAuthProvider>
+    </Provider>
+  </ErrorBoundary>
 )
