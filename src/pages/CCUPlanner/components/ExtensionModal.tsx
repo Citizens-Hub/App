639c8972282cb6e74512d211638b6df065ea74ed
EduarteXD <i@m.oxdl.cn>
2025-07-16 21:57:20 +0800

    docs: update extension download links to version 1.0.2

--- src/pages/CCUPlanner/components/ExtensionModal.tsx
+++ src/pages/CCUPlanner/components/ExtensionModal.tsx
@@ -75,7 +75,7 @@ export default function ExtensionModal({ open, onClose }: ExtensionModalProps) {
                   <FormattedMessage id="extensionModal.firefoxLink" defaultMessage="Firefox Add-ons" />
                 </Link>
                 <Link 
-                  href="/citizens_hub-1.0.0.xpi" 
+                  href="/citizens_hub-1.0.2.xpi" 
                   target="_blank" 
                   rel="noopener noreferrer"
                 >
