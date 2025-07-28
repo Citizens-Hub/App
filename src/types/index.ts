4fdf9c9e70d31f7f14acbdc7cb64b6911ca1f8be
EduarteXD <i@m.oxdl.cn>
2025-07-28 17:19:52 +0800

    refactor: remove unused handleAddToCart function from CcuEdgeData interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -158,7 +158,6 @@ export interface CcuEdgeData {
   wbHistory: WbHistoryData[];
   hangarItems: HangarItem[];
   importItems: ImportItem[];
-  handleAddToCart: (from: number, to: number) => void;
 }
 
 export interface WbHistoryData {
