828dcb07527138fc87ae66c8aa999e0902c60b2b
EduarteXD <i@m.oxdl.cn>
2025-11-02 12:04:43 +0800

    refactor: remove unused dependencies and logic in BlankPageAd component

--- src/components/BlankPageAd.tsx
+++ src/components/BlankPageAd.tsx
@@ -1,24 +1,14 @@
 import { Adsense } from '@ctrl/react-adsense';
-import { useLocation } from 'react-router';
-import { useEffect, useState } from 'react';
 
 export default function BlankPageAd() {
-  const { pathname } = useLocation();
-  const [key, setKey] = useState(0);
-
-  // 当pathname变化时更新key以刷新广告
-  useEffect(() => {
-    setKey(prevKey => prevKey + 1);
-  }, [pathname]);
 
   return (
     <div className="w-full max-h-[400px]">
       <Adsense
-        key={key}
         client={import.meta.env.VITE_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}
         slot="7853733031"
         style={{ display: 'inline-block', width: '100%', height: '450px' }}
-        // format="auto"
+        format=""
         adTest={import.meta.env.DEV ? "on" : "off"}
       />
     </div>
