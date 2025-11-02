c89f61947cc7fd328d03254db3c9a9aa68fc56f0
EduarteXD <i@m.oxdl.cn>
2025-11-02 11:49:00 +0800

    feat(BlankPageAd): add responsive attribute to ad component

--- src/components/BlankPageAd.tsx
+++ src/components/BlankPageAd.tsx
@@ -19,6 +19,7 @@ export default function BlankPageAd() {
       style={{ display: 'block' }}
       format="auto"
       adTest={import.meta.env.DEV ? "on" : "off"}
+      responsive="true"
     />
   );
 }
