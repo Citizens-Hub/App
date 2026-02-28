493fdf71ebb7189f0ea42a88084477e5c737a6a5
EduarteXD <i@m.oxdl.cn>
2026-03-01 00:07:22 +0800

    build: enable source maps for development

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(() => {
       visualizer(),
     ],
     build: {
-      sourcemap: false,
+      sourcemap: true,
       manifest: true,
       rollupOptions: {
         output: {
