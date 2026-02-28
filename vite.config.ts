a71125f9435468f69f0d51cbc7421ec90cebd673
EduarteXD <i@m.oxdl.cn>
2026-03-01 00:09:44 +0800

    build: disable sourcemaps in production build

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(() => {
       visualizer(),
     ],
     build: {
-      sourcemap: true,
+      sourcemap: false,
       manifest: true,
       rollupOptions: {
         output: {
